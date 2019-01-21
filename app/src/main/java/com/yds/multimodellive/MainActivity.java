df64ad999b60759cb800eda380af40e757b678c0
ydswinter <ydswinter@outlook.com>
2019-01-21 21:56:58 +0800

    登录界面设计

--- app/src/main/java/com/yds/multimodellive/MainActivity.java
+++ app/src/main/java/com/yds/multimodellive/MainActivity.java
@@ -1,5 +1,6 @@
 package com.yds.multimodellive;
 
+import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private DrawerLayout drawer;//抽屉菜单
     private ImageButton open;//菜单按钮
     private RelativeLayout feedback;//反馈选项
+    private ImageView picture;//头像
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,9 +31,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         drawer = findViewById(R.id.drawer);
         open = findViewById(R.id.open);
         feedback = findViewById(R.id.item_feedback);
+        picture = findViewById(R.id.picture);
 
         //为控件设置监听器
         feedback.setOnClickListener(this);
+        picture.setOnClickListener(this);
         open.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,6 +50,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()){
             case R.id.item_feedback:
 
+                break;
+            case R.id.picture:
+                startActivity(new Intent(MainActivity.this,SignInActivity.class));
                 break;
         }
     }
---
+++ app/src/main/java/com/yds/multimodellive/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.yds.multimodellive;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        },3000);
+    }
+}
